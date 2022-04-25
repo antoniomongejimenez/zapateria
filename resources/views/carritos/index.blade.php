07d1fd49250f7768f9faa30647d511fd32e7ed50
antoniomongejimenez <antonio.monge.jimenez@iesdonana.org>
2022-04-25 13:12:22 +0200

    Pequeños cambios

--- resources/views/carritos/index.blade.php
+++ resources/views/carritos/index.blade.php
@@ -1,65 +1,74 @@
-<x-carritos>
-    <table class="table-auto">
-        <thead>
-            <th class="px-6 py-2 text-gray-500">
-                Zapato
-            </th>
-            <th class="px-6 py-2 text-gray-500">
-                Cantidad
-            </th>
-            <th class="px-6 py-2 text-gray-500">
-                Precio individual
-            </th>
-            <th class="px-6 py-2 text-gray-500">
-                Importe
-            </th>
-            <th class="px-6 py-2 text-gray-500">
-                Sumar-restar
-            </th>
-        </thead>
-        <tbody>
-            @foreach ($carritos as $carrito)
-                <tr>
-                    <td class="px-6 py-2">{{ $carrito->zapato->denominacion}}</td>
-                    <td class="px-6 py-2">{{ $carrito->cantidad }}</td>
-                    <td class="px-6 py-2">{{ $carrito->zapato->precio }} euros</td>
-                    <td class="px-6 py-2">{{ $carrito->zapato->precio * $carrito->cantidad }} euros</td>
-                    <td class="px-6 py-4">
-                        <div class="text-sm text-gray-900">
-                            <div>
-                                <form action="{{route('carrito.sumar', $carrito->zapato)}}" method="post">
-                                    @csrf
-                                    @method('POST')
-                                    <button class="inline-flex text-black h-6 px-3 justify-center items-center" type="submit">+</button>
-                                </form>
-                            </div>
+<x-app-layout>
 
-                            <div>
-                                <form action="{{route('carrito.restar', $carrito->zapato)}}" method="post">
-                                    @csrf
-                                    @method('POST')
-                                    <button class="inline-flex text-black h-6 px-3 justify-center items-center" type="submit"> - </button>
-                                </form>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Carrito') }}
+        </h2>
+    </x-slot>
+
+    <x-carritos>
+        <table class="table-auto">
+            <thead>
+                <th class="px-6 py-2 text-gray-500">
+                    Zapato
+                </th>
+                <th class="px-6 py-2 text-gray-500">
+                    Cantidad
+                </th>
+                <th class="px-6 py-2 text-gray-500">
+                    Precio individual
+                </th>
+                <th class="px-6 py-2 text-gray-500">
+                    Importe
+                </th>
+                <th class="px-6 py-2 text-gray-500">
+                    Sumar-restar
+                </th>
+            </thead>
+            <tbody>
+                @foreach ($carritos as $carrito)
+                    <tr>
+                        <td class="px-6 py-2">{{ $carrito->zapato->denominacion}}</td>
+                        <td class="px-6 py-2">{{ $carrito->cantidad }}</td>
+                        <td class="px-6 py-2">{{ $carrito->zapato->precio }} euros</td>
+                        <td class="px-6 py-2">{{ $carrito->zapato->precio * $carrito->cantidad }} euros</td>
+                        <td class="px-6 py-4">
+                            <div class="text-sm text-gray-900">
+                                <div>
+                                    <form action="{{route('carrito.sumar', $carrito->zapato)}}" method="post">
+                                        @csrf
+                                        @method('POST')
+                                        <button class="inline-flex text-black h-6 px-3 justify-center items-center" type="submit">+</button>
+                                    </form>
+                                </div>
+
+                                <div>
+                                    <form action="{{route('carrito.restar', $carrito->zapato)}}" method="post">
+                                        @csrf
+                                        @method('POST')
+                                        <button class="inline-flex text-black h-6 px-3 justify-center items-center" type="submit"> - </button>
+                                    </form>
+                                </div>
                             </div>
-                        </div>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <div>
-        <form action="{{route('carrito.facturar')}}" method="post">
-            @csrf
-            @method('POST')
-            <button class="inline-flex bg-green-500 text-black h-6 px-3 justify-center items-center" type="submit"> Realizar pedido</button>
-        </form>
-    </div>
-    <div class="mt-5">
-        <form action="{{route('carrito.vaciar')}}" method="post">
-            @csrf
-            @method('POST')
-            <button class="inline-flex bg-red-500 text-black h-6 px-3 justify-center items-center" type="submit"> Vaciar carrito</button>
-        </form>
-    </div>
-    <a href="/dashboard" class="mt-4 text-blue-900 hover:underline">Volver</a>
-</x-carritos>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <div>
+            <form action="{{route('carrito.facturar')}}" method="post">
+                @csrf
+                @method('POST')
+                <button class="bg-green-500 text-black px-5 py-2" type="submit"> Realizar pedido</button>
+            </form>
+        </div>
+        <div class="mt-5">
+            <form action="{{route('carrito.vaciar')}}" method="post">
+                @csrf
+                @method('POST')
+                <button class="bg-red-500 text-black px-7 py-2" type="submit"> Vaciar carrito</button>
+            </form>
+        </div>
+    </x-carritos>
+
+</x-app-layout>
--- resources/views/zapatos/index.blade.php
+++ resources/views/zapatos/index.blade.php
@@ -1,40 +1,47 @@
-<x-zapatos>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Zapatos') }}
+        </h2>
+    </x-slot>
 
-    <table class="table-auto">
-        <thead>
-            <th class="px-6 py-2 text-gray-500">
-                Código
-            </th>
-            <th class="px-6 py-2 text-gray-500">
-                Zapato
-            </th>
-            <th class="px-6 py-2 text-gray-500">
-                Precio
-            </th>
-            <th class="px-6 py-2 text-gray-500">
-                Añadir al carrito
-            </th>
+    <x-zapatos>
 
-        </thead>
-        <tbody>
-            @foreach ($zapatos as $zapato)
-                <tr>
-                    <td class="px-6 py-2">{{ $zapato->codigo}}</td>
-                    <td class="px-6 py-2">{{ $zapato->denominacion}}</td>
-                    <td class="px-6 py-2">{{ $zapato->precio }}</td>
-                    <td class="px-6 py-2">
-                        <div class="text-sm text-gray-900 ">
-                            <form action="{{ route('carrito.meter', $zapato) }}" method="POST">
-                                @csrf
-                                @method('POST')
-                                <button type="submit" class="px-4 py-1 text-sm text-white bg-red-400 rounded">Añadir al carrito</button>
-                            </form>
-                        </div>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <a href="/carritos" class="mt-4 text-blue-900 hover:underline">Carrito</a>
-    <a href="/dashboard" class="mt-4 text-blue-900 hover:underline">Volver</a>
-</x-zapatos>
+        <table class="table-auto">
+            <thead>
+                <th class="px-6 py-2 text-gray-500">
+                    Código
+                </th>
+                <th class="px-6 py-2 text-gray-500">
+                    Zapato
+                </th>
+                <th class="px-6 py-2 text-gray-500">
+                    Precio
+                </th>
+                <th class="px-6 py-2 text-gray-500">
+                    Añadir al carrito
+                </th>
+
+            </thead>
+            <tbody>
+                @foreach ($zapatos as $zapato)
+                    <tr>
+                        <td class="px-6 py-2">{{ $zapato->codigo}}</td>
+                        <td class="px-6 py-2">{{ $zapato->denominacion}}</td>
+                        <td class="px-6 py-2">{{ $zapato->precio }}</td>
+                        <td class="px-6 py-2">
+                            <div class="text-sm text-gray-900 ">
+                                <form action="{{ route('carrito.meter', $zapato) }}" method="POST">
+                                    @csrf
+                                    @method('POST')
+                                    <button type="submit" class="px-5 py-2 text-black bg-green-400">Añadir al carrito</button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </x-zapatos>
+
+</x-app-layout>
